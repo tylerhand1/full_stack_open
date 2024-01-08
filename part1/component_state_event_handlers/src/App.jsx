8fc360b760264c60c775f15d4370fa3a88352bf3
tylerhand1 <tyha7462@colorado.edu>
2024-01-08 13:07:49 -0700

    Part 1 Component state, event handlers Finished all sections

--- part1/component_state_event_handlers/src/App.jsx
+++ part1/component_state_event_handlers/src/App.jsx
@@ -1,17 +1,32 @@
 import {useState} from 'react'
 
+const Display = ({counter}) => <div>{counter}</div>
+
+const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
+
 const App = () => {
   const [counter, setCounter] = useState(0)
 
-  setTimeout(
-    () => setCounter(counter + 1),
-    1000
-  )
-
-  console.log('rendering...', counter)
+  const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
+  const setToZero = () => setCounter(0)
 
   return (
-    <div>{counter}</div>
+    <div>
+      <Display counter={counter} />
+      <Button 
+        onClick={increaseByOne}
+        text='plus'  
+      />
+      <Button 
+        onClick={setToZero}
+        text='zero'
+      />
+      <Button
+        onClick={decreaseByOne}
+        text='minus'
+      />
+    </div>
   )
 }
 
