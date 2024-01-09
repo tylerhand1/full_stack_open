a6871dafaad55cd0f6fb4583fe88f1ea838dcfd6
tylerhand1 <tyha7462@colorado.edu>
2024-01-09 15:11:41 -0700

    Exercise 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -13,25 +13,51 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
 
-  const increment = (operand, func) => {
-    const updatedOperand = operand + 1
-    func(updatedOperand)
+  const handleGoodClick = () => {
+    const updatedGood = good + 1
+    setGood(updatedGood)
+    setAll(updatedGood + neutral + bad)
+  }
+
+  const handleNeutralClick = () => {
+    const updatedNeutral = neutral + 1
+    setNeutral(updatedNeutral)
+    setAll(good + updatedNeutral + bad)
+  }
+
+  const handleBadClick = () => {
+    const updatedBad = bad + 1
+    setBad(updatedBad)
+    setAll(good + neutral + updatedBad)
+  }
+
+  const getAvg = () => {
+    const sum = (good * 1.0) + (neutral * 0) + (bad * -1)
+    return sum / all
+  }
+
+  const getPositive = () => {
+    return good / all * 100
   }
 
   return (
     <div>
       <div>
         <h1>give feedback</h1>
-        <Button handleClick={() => increment(good, setGood)} text="good" />
-        <Button handleClick={() => increment(neutral, setNeutral)} text="neutral" />
-        <Button handleClick={() => increment(bad, setBad)} text="bad" />
+        <Button handleClick={handleGoodClick} text="good" />
+        <Button handleClick={handleNeutralClick} text="neutral" />
+        <Button handleClick={handleBadClick} text="bad" />
       </div>
       <div>
         <h2>statistics</h2>
         <p>good {good}</p>
         <p>neutral {neutral}</p>
         <p>bad {bad}</p>
+        <p>all {all}</p>
+        <p>average {all === 0 ? 0 : getAvg()}</p>
+        <p>positive {all === 0 ? 0 : getPositive()}%</p>
       </div>
     </div>
   )
