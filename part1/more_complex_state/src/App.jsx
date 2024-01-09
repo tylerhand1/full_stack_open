a0d5c9a868e4c2f0845f8c5c9a24c9ebc01393ae
tylerhand1 <tyha7462@colorado.edu>
2024-01-09 14:33:16 -0700

    Part 1 A more complex state, debugging React apps Sections Conditional rendering to Do not define components within components

--- part1/more_complex_state/src/App.jsx
+++ part1/more_complex_state/src/App.jsx
@@ -1,39 +1,26 @@
 import {useState} from 'react'
 
+const Display = props => <div>{props.value}</div>
+
+const Button = ({handleClick, text}) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
 const App = () => {
-  const [clicks, setClicks] = useState({
-    left: 0, right: 0
-  })
-  const [allClicks, setAll] = useState([])
-  const [total, setTotal] = useState(0)
+  const [value, setValue] = useState(10)
 
-  // Forbidden to mutate state directly in React - https://stackoverflow.com/a/40309023
-  const handleLeftClick = () => {
-    setAll(allClicks.concat('L'))
-    const updatedLeft = clicks.left + 1 // State update is async --> need this to ensure clicks and total are correct
-    setClicks({...clicks, left: updatedLeft})
-    setTotal(updatedLeft + clicks.right)
-  }
-
-  const handleRightClick = () => {
-    setAll(allClicks.concat('R'))
-    const updatedRight = clicks.right + 1
-    setClicks({...clicks, right: updatedRight})
-    setTotal(clicks.left + updatedRight)
+  const setToValue = (newValue) => {
+    setValue(newValue)
   }
 
   return (
     <div>
-      {clicks.left}
-      <button onClick={handleLeftClick}>
-        left
-      </button>
-      <button onClick={handleRightClick}>
-        right
-      </button>
-      {clicks.right}
-      <p>{allClicks.join(' ')}</p>
-      <p>total {total}</p>
+      <Display value={value} />
+      <Button handleClick={() => setToValue(1000)} text="thousand" />
+      <Button handleClick={() => setToValue(0)} text="zero" />
+      <Button handleClick={() => setToValue(value + 1)}  text="increment" />
     </div>
   )
 }
