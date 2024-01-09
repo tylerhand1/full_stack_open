eb075ad3972b179e2045ddb4c2d9f0a7d3e171a2
tylerhand1 <tyha7462@colorado.edu>
2024-01-09 13:28:39 -0700

    Part 1 A more complex state, debugging React apps Sections Handling arrays & Update of the state is asynchronous

--- part1/more_complex_state/src/App.jsx
+++ part1/more_complex_state/src/App.jsx
@@ -4,13 +4,22 @@ const App = () => {
   const [clicks, setClicks] = useState({
     left: 0, right: 0
   })
+  const [allClicks, setAll] = useState([])
+  const [total, setTotal] = useState(0)
+
   // Forbidden to mutate state directly in React - https://stackoverflow.com/a/40309023
   const handleLeftClick = () => {
-    setClicks({...clicks, left: clicks.left + 1})
+    setAll(allClicks.concat('L'))
+    const updatedLeft = clicks.left + 1 // State update is async --> need this to ensure clicks and total are correct
+    setClicks({...clicks, left: updatedLeft})
+    setTotal(updatedLeft + clicks.right)
   }
 
   const handleRightClick = () => {
-    setClicks({...clicks, right: clicks.right + 1})
+    setAll(allClicks.concat('R'))
+    const updatedRight = clicks.right + 1
+    setClicks({...clicks, right: updatedRight})
+    setTotal(clicks.left + updatedRight)
   }
 
   return (
@@ -23,6 +32,8 @@ const App = () => {
         right
       </button>
       {clicks.right}
+      <p>{allClicks.join(' ')}</p>
+      <p>total {total}</p>
     </div>
   )
 }
