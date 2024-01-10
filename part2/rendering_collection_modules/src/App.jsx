d761e316a7a2dc11aab67bb168be3ee6c7fa6e9e
tylerhand1 <tyha7462@colorado.edu>
2024-01-10 15:00:34 -0700

    Part 2 Rendering a collection, modules Sections console.log to Map

--- part2/rendering_collection_modules/src/App.jsx
+++ part2/rendering_collection_modules/src/App.jsx
@@ -1,5 +1,18 @@
-const App = () => {
-  
+const App = (props) => {
+  const {notes} = props
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(note => 
+          <li key={note.id}>
+            {note.content}
+          </li>
+        )}
+      </ul>
+    </div>
+  )
 }
 
 export default App
