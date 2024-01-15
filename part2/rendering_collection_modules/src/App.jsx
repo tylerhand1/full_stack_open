5c74ba76256058ba95984f57b75e3adb7ff82978
tylerhand1 <tyha7462@colorado.edu>
2024-01-15 14:21:54 -0700

    Part 2 Getting data from server Section Effect-hooks

--- part2/rendering_collection_modules/src/App.jsx
+++ part2/rendering_collection_modules/src/App.jsx
@@ -1,10 +1,21 @@
-import {useState} from 'react'
+import {useEffect, useState} from 'react'
+import axios from 'axios'
 import Note from './components/Note'
 
 const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
+  const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promise fulfilled')
+        setNotes(response.data)
+      })
+  }, [])
 
   const addNote = (event) => {
     event.preventDefault()
--- part2/rendering_collection_modules/src/main.jsx
+++ part2/rendering_collection_modules/src/main.jsx
@@ -1,16 +1,4 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-import axios from 'axios'
-
-import App from './App'
-
-axios
-  .get('http://localhost:3001/notes')
-  .then(response => {
-    const notes = response.data
-    ReactDOM.createRoot(document.getElementById('root')).render(
-      <App notes={notes} />
-    )    
-  })
-
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
