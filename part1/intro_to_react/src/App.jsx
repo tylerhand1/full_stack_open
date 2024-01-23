892db0e06a4fbd4e2a7e810c822d776e21646856
tylerhand1 <tyha7462@colorado.edu>
2024-01-22 17:11:20 -0700

    Adding files from part1 forgotten to add

---
+++ part1/intro_to_react/src/App.jsx
@@ -0,0 +1,32 @@
+const Hello = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <p>Hello {props.name}, you are {props.age} years old</p>
+    </div>
+  )
+}
+
+const App = () => {
+  // const now = new Date()
+  // const a = 10
+  // const b = 20
+  // console.log(now, a+b)
+
+  const name = 'Peter'
+  const age = 10
+
+  return (
+    <>
+    {/* <p>Hello world, it is now {now.toString()}</p>
+      <p>
+        {a} plus {b} is {a + b}
+      </p> */}
+    <h1>Greetings</h1>
+    <Hello name='Maya' age={26 + 10} />
+    <Hello name={name} age={age} />
+  </>
+  )
+}
+
+export default App
