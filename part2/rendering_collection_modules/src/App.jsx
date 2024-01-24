0402feeee2dece446822fa16c364a8463e794968
tylerhand1 <tyha7462@colorado.edu>
2024-01-24 15:17:57 -0700

    Part 2 Altering data in server Section Extracting Communication with the Backend into a Separate Module

--- part2/rendering_collection_modules/src/App.jsx
+++ part2/rendering_collection_modules/src/App.jsx
@@ -1,19 +1,18 @@
 import {useEffect, useState} from 'react'
-import axios from 'axios'
 import Note from './components/Note'
+import noteService from './services/notes'
 
-const App = (props) => {
+const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
 
   useEffect(() => {
     console.log('effect')
-    axios
-      .get('http://localhost:3001/notes')
-      .then(response => {
-        console.log('promise fulfilled')
-        setNotes(response.data)
+    noteService
+      .getAll()
+      .then(initialNotes => {
+        setNotes(initialNotes)
       })
   }, [])
 
@@ -22,27 +21,24 @@ const App = (props) => {
     const noteObject = {
       content: newNote,
       important: Math.random() < 0.5,
-      id: notes.length + 1
+      id: `${notes.length + 1}`
     }
-
-    axios
-      .post('http://localhost:3001/notes', noteObject)
-      .then(response => {
-        console.log(response)
-        setNotes(notes.concat(noteObject))
+    console.log(noteObject)
+    noteService
+      .create(noteObject)
+      .then(returnedNote => {
+        setNotes(notes.concat(returnedNote))
         setNewNote('')
       })
   }
 
   const toggleImportance = id => {
-    const url = `http://localhost:3001/notes/${id}`
     const note = notes.find(n => n.id === id)
     const changedNote = {...note, important: !note.important}
-
-    axios
-      .put(url, changedNote)
-      .then(response => {
-        setNotes(notes.map(n => n.id !== id ? n : response.data))
+    noteService
+      .update(id, changedNote)
+      .then(returnedNote => {
+        setNotes(notes.map(n => n.id !== id ? n : returnedNote))
       })
   }
 
