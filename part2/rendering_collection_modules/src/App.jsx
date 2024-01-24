9b2b2315128f1e8a5b34dcc3bd16517a754daa9d
tylerhand1 <tyha7462@colorado.edu>
2024-01-24 14:51:55 -0700

    Part 2 Altering data in server Section Changing the Importance of Notes

--- part2/rendering_collection_modules/src/App.jsx
+++ part2/rendering_collection_modules/src/App.jsx
@@ -34,6 +34,18 @@ const App = (props) => {
       })
   }
 
+  const toggleImportance = id => {
+    const url = `http://localhost:3001/notes/${id}`
+    const note = notes.find(n => n.id === id)
+    const changedNote = {...note, important: !note.important}
+
+    axios
+      .put(url, changedNote)
+      .then(response => {
+        setNotes(notes.map(n => n.id !== id ? n : response.data))
+      })
+  }
+
   const handleNoteChange = (event) => {
     // console.log(event.target.value)
     setNewNote(event.target.value)
@@ -53,7 +65,11 @@ const App = (props) => {
       </div>
       <ul>
         {notesToShow.map(note => 
-          <Note key={note.id} note={note} />
+          <Note 
+            key={note.id} 
+            note={note} 
+            toggleImportance={() => toggleImportance(note.id)}
+          />
         )}
       </ul>
       <form onSubmit={addNote}>
