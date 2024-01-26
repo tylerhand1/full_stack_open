6428be79db24e7813084b369978d0b981c968eb7
tylerhand1 <tyha7462@colorado.edu>
2024-01-26 11:16:42 -0700

    Part 2 Adding styles to React App Improved error message

--- part2/rendering_collection_modules/src/App.jsx
+++ part2/rendering_collection_modules/src/App.jsx
@@ -2,10 +2,23 @@ import {useEffect, useState} from 'react'
 import Note from './components/Note'
 import noteService from './services/notes'
 
+const Notification = ({message}) => {
+  if(message === null) {
+    return null
+  }
+
+  return (
+    <div className='error'>
+      {message}
+    </div>
+  )
+}
+
 const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState('some error happened...')
 
   useEffect(() => {
     console.log('effect')
@@ -41,9 +54,12 @@ const App = () => {
         setNotes(notes.map(n => n.id !== id ? n : returnedNote))
       })
       .catch(error => {
-        alert(
-          `the note ${note.content} was already deleted from server`
+        setErrorMessage(
+          `Note '${note.content}' was already removed from the server`
         )
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
         setNotes(notes.filter(n => n.id !== id))
       })
   }
@@ -60,6 +76,7 @@ const App = () => {
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all'}
