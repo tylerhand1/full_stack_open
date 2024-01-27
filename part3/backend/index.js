4cfadf26dea94f13c760edcd15b3544924432e40
tylerhand1 <tyha7462@colorado.edu>
2024-01-27 09:40:38 -0700

    Part 3 Node.js and Express Simple web server

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -1,1 +1,10 @@
-console.log('hello world')
+const http = require('http')
+
+const app = http.createServer((request, response) => {
+    response.writeHead(200, { 'Content-Type': 'text/html' })
+    response.end('<h1>Hello world</h1>')
+})
+
+const PORT = 3001
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`)
