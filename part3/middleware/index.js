23722fa414f80cea9b993f18da442c5337ad9af1
tylerhand1 <tyha7462@colorado.edu>
2024-02-02 11:22:32 -0700

    Create files

---
+++ part3/middleware/index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+
+const requestLogger = (req, res, next) => {
+    console.log(`Method: ${req.method}`)
+    console.log(`Path: ${req.path}`)
+    console.log(`Body: ${req.body}`)
+    console.log(`---`)
+    next()
+}
+
+app.use(requestLogger)
+
+app.get('/', (req, res) => {
+    res.send(`<h1>Hello world!</h1>`)
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`)
+})
